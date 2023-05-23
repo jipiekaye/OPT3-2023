7b63aa5727a8f1a0200fade18208f45e37f09150
Jip Gardien <jipdegrote@gmail.com>
2023-05-23 11:42:33 +0200

    Melding basis aangelegd

--- OPT3-2023/src/main/java/com/jipiekaye/opt32023/Melding.java
+++ OPT3-2023/src/main/java/com/jipiekaye/opt32023/Melding.java
@@ -1,4 +1,11 @@
 package com.jipiekaye.opt32023;
 
 public class Melding {
+    private String bericht;
+    public Melding(String titel) {
+        this.bericht = titel + " moet nu gedaan worden!";
+    }
+    public void popup(){
+        System.out.println(bericht);
+    }
 }
